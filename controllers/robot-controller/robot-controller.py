fc02adbc057dc2ccff206bbe76cdd0f87191a1f8
marco-vb <marco.vilasboas03@gmail.com>
2025-02-10 15:56:57 +0000

    Added bumper sensor to detect collisions of robot in Webots.

--- controllers/robot-controller/robot-controller.py
+++ controllers/robot-controller/robot-controller.py
@@ -1,106 +1,123 @@
-from controller import Robot, Motor
-import gymnasium
+from controller import Supervisor
+import gymnasium as gym
 import numpy as np
-
-# import check_env
-from gymnasium.utils.env_checker import check_env
+from stable_baselines3 import PPO
 import logging
 
 
-class RobotEnv(gymnasium.Env):
+class CorridorNavigationEnv(Supervisor, gym.Env):
     def __init__(self):
-        super(RobotEnv, self).__init__()
+        super().__init__()
+        self.timestep = int(self.getBasicTimeStep())
 
         """
-        Webots constants
+        Define action and observation space.
         """
-        self.TIME_STEP = 64
-        self.MAX_SPEED = 6.28
-
-        """
-        Setup robot and devices
-        """
-        self.robot = Robot()
-        self.name = self.robot.getName()
-        self.left_motor = self.robot.getDevice("left wheel motor")
-        self.right_motor = self.robot.getDevice("right wheel motor")
-        self.left_motor.setPosition(float("inf"))
-        self.right_motor.setPosition(float("inf"))
-
-        self.lidar = self.robot.getDevice("LDS-01")
-        self.lidar.n_samples = 360
-        self.lidar.low = np.full(self.lidar.n_samples, 0.0, dtype=np.float32)
-        self.lidar.high = np.full(self.lidar.n_samples, 10.0, dtype=np.float32)
-        self.lidar.enable(self.TIME_STEP)
-        self.lidar.enablePointCloud()
-
-        """
-        Setup variables
-        """
-        self.collided = False
-        self.finished = False
-        self.timestep = 0
-        # self.timelimit = 6000
-
-        self.speed = self.MAX_SPEED / 2
-
+        self.MAX_SPEED = 5
         self.actions = [
-            (self.speed, self.speed),  # move forward
-            (self.speed, self.speed / 2),  # turn right
-            (self.speed / 2, self.speed),  # turn left
+            (self.MAX_SPEED, self.MAX_SPEED),
+            (self.MAX_SPEED, self.MAX_SPEED / 2),
+            (self.MAX_SPEED / 2, self.MAX_SPEED),
         ]
+        self.num_to_action = lambda i: self.actions[i]
 
-        self.action_space = gymnasium.spaces.Discrete(len(self.actions))
-        self.num_to_action = lambda x: self.actions[x]
+        self.action_space = gym.spaces.Discrete(len(self.actions))
 
-        self.observation_space = gymnasium.spaces.Box(
-            low=self.lidar.low,
-            high=self.lidar.high,
-            shape=(self.lidar.n_samples,),
-            dtype=np.float32,
+        self.n_samples = 360
+        self.observation_space = gym.spaces.Box(
+            low=0, high=10, shape=(self.n_samples,), dtype=np.float32
         )
 
+        self.done = False
+        self.truncated = False
+
+        """
+        Setup wheels and lidar devices.
+        """
+        self.wheels = []
+        for name in ["left wheel motor", "right wheel motor"]:
+            wheel = self.getDevice(name)
+            wheel.setPosition(float("inf"))
+            wheel.setVelocity(0)
+            self.wheels.append(wheel)
+
+        self.lidar = self.getDevice("Lidar")
+        self.lidar.enable(self.timestep)
+        self.lidar.enablePointCloud()
+
+        self.robot = self.getFromDef("Turtle0")
+        self.bumper = self.getDevice("Bumper")
+        self.bumper.enable(self.timestep)
+
+        logging.basicConfig(
+            filename="Turtle0",
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+        )
+
+        logging.info("env initialized")
+
+    def reset(self, seed=None):
+        self.done = False
+
+        # robot_node = self.getFromDef("Turtle0")
+        # if robot_node:
+        #     robot_translation = robot_node.getField("translation")
+        #     robot_rotation = robot_node.getField("rotation")
+        #     robot_translation.setSFVec3f([0.214, 0, 0])
+        #     robot_rotation.setSFRotation([0.0, 0.0, 1.0, 0])
+
+        # for wheel in self.__wheels:
+        #     wheel.setVelocity(0)
+
+        # Reset robot position
+        self.simulationResetPhysics()
+        super().step(self.timestep)
+
+        return self.get_obs(), {}
+
+    def get_obs(self):
+        lidar_values = np.array(self.lidar.getRangeImage(), dtype=np.float32)
+        return np.clip(lidar_values, 0, 10)
+
+    def calculate_reward(self, lidar_values, action):
+        return 0
+
     def step(self, action):
-        pass
+        l, r = self.num_to_action(action)
+        self.wheels[0].setVelocity(l)
+        self.wheels[1].setVelocity(r)
 
-    def reset(self):
-        pass
+        super().step(self.timestep)
 
-    def render(self):
-        pass
+        if self.bumper.getValue() == 1:
+            print("Collision detected")
+            logging.info("Collision detected")
 
-    def close(self):
-        pass
+        lidar_values = self.get_obs()
+        reward = self.calculate_reward(lidar_values, action)
 
-    def seed(self):
-        pass
+        self.done = False
+        self.truncated = False
 
-    def get_state(self):
-        pass
-
-    def get_reward(self):
-        pass
-
-    def get_done(self):
-        pass
-
-    def get_info(self):
-        pass
+        return lidar_values, reward, self.done, self.truncated, {}
 
 
 def main():
-    env = RobotEnv()
-    check_env(env)
+    env = CorridorNavigationEnv()
+    model = PPO("MlpPolicy", env, verbose=0, device="cpu")
 
-    logging.basicConfig(
-        filename=f"{env.name}.log",
-        level=logging.DEBUG,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
-    logger = logging.getLogger(__name__)
-
-    while env.robot.step(env.TIME_STEP) != -1:
-        pass
+    obs, _ = env.reset()
+    rewards = []
+    acc = 0
+    for _ in range(30000):
+        action, _ = model.predict(obs)
+        obs, reward, done, _, _ = env.step(action)
+        acc += reward
+        if done:
+            rewards.append(acc)
+            acc = 0
+            obs, _ = env.reset()
 
 
 if __name__ == "__main__":
