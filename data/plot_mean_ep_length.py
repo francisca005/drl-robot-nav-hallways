e3feba690bbf8c1acb944e36092a7ceccba4993f
Marco Vilas Boas <marco.vilasboas03@gmail.com>
2025-07-02 16:50:20 +0100

    Added code to plot standard RL metrics.

--- data/plot_mean_ep_length.py
+++ data/plot_mean_ep_length.py
@@ -6,10 +6,10 @@ df = reader.scalars
 
 print(df.head())
 
-plt.plot(df["step"], df["rollout/ep_rew_mean"])
+plt.plot(df["step"], df["rollout/ep_len_mean"])
 plt.xlabel("Timesteps")
-plt.ylabel("Mean Episode Reward")
-plt.title("Reward Over Time in Training")
+plt.ylabel("Mean Episode Length")
+plt.title("Mean Episode Length Over Time in Training")
 plt.grid(True)
 plt.savefig("reward_plot.pdf", bbox_inches="tight")
 plt.show()
