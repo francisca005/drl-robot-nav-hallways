fa8769de582aebfe45cc1d37620c6b85d2b1c43d
Francisca <francisca0cerqueira@gmail.com>
2026-05-18 07:34:56 +0100

    Checkpoint completo

--- src/wheelchair_env.py
+++ src/wheelchair_env.py
@@ -6,20 +6,21 @@ import numpy as np
 import zmq
 from gymnasium.spaces import Box
 
-
+#criar um ambiente Gymnasium
 class WheelchairEnv(gym.Env):
     def __init__(self, env_id: int):
         super(WheelchairEnv, self).__init__()
 
-        context = zmq.Context()
-        self.socket = context.socket(zmq.REQ)
+        context = zmq.Context() #ligação com robt_client.py
+        self.socket = context.socket(zmq.REQ) #pedidos
         self.socket.bind(f"tcp://127.0.0.1:{5550 + env_id}")
 
         self.env_id = env_id
 
+        #6 ações discretas
         self.action_space = gym.spaces.Discrete(6)
 
-        v, w = 1, 3
+        v, w = 1, 3 # 1 lienar,3 angular
         self.to_action = lambda x: (
             [
                 [v, 0],    # Forward
@@ -31,6 +32,7 @@ class WheelchairEnv(gym.Env):
             ]
         )[x]
 
+        #360 LiDAR (de 0 a 10) + 1 valor da ação anterior (de 0 a 5)
         self.observation_space = Box(
             low=np.concatenate([np.full(360, 0.0), [0]]),
             high=np.concatenate([np.full(360, 10.0), [5]]),
@@ -39,35 +41,35 @@ class WheelchairEnv(gym.Env):
         self.obs_shape = self.observation_space.shape
 
         self.no_obs()
-        self.prev_action = 0
-        self.prev_pref = 0.0
-        self.time_step = 0
-        self.time_limit = 5_000
-        self.commitment_threshold = 2.5
+        self.prev_action = 0 #ação anterior a 0
+        self.prev_pref = 0.0 #preferência anterior de navegação
+        self.time_step = 0 #steps desde o início
+        self.time_limit = 5_000 #max por episódio
+        self.commitment_threshold = 2.5 #reward de navegação
 
         self.timeout_reset_command = np.array([-999.0, -999.0], dtype=np.float32)
 
     def step(self, action: int):
-        self.prev_action = action
-        action_values = self.to_action(action)
+        self.prev_action = action #ação escolhida
+        action_values = self.to_action(action) #passar a velocidades
 
-        reward = self.get_reward(self.prev_lidar, action_values)
+        reward = self.get_reward(self.prev_lidar, action_values) #reward = estado anterior + ação escolhida
 
-        obs = self.send_action_get_obs(action_values)
+        obs = self.send_action_get_obs(action_values) #envia ação robot_client.py
 
         if obs.collided:
-            reward += self.collision_reward()
+            reward += self.collision_reward() #penalização de colisão, agora a -10
         elif obs.goal_reached:
-            reward += self.goal_reward()
+            reward += self.goal_reward() #reward de objetivo, agora a 0
 
-        self.prev_lidar = obs.lidar
+        self.prev_lidar = obs.lidar #atualiza LiDAR para o prox step
         self.time_step += 1
 
-        terminated = obs.collided or obs.goal_reached
-        truncated = self.time_step >= self.time_limit
+        terminated = obs.collided or obs.goal_reached #colisão ou objetivo
+        truncated = self.time_step >= self.time_limit #tempo limite excedido
 
         if truncated and not terminated:
-            reward -= 10
+            reward -= 10 #se deu timeout é penalizado
 
 
         info = {
@@ -78,6 +80,7 @@ class WheelchairEnv(gym.Env):
 
         return obs.to_array(), reward, terminated, truncated, info
 
+    #timeout lógica antiga
     def request_timeout_reset(self) -> RobotState:
         """
         Inform the Webots controller that the current episode ended by timeout.
@@ -93,18 +96,18 @@ class WheelchairEnv(gym.Env):
     def get_reward(self, obs: np.ndarray, action: Tuple[int, int]) -> float:
         v, w = action
 
-        r_distance = 1.0 if v > 0 else 0.0
+        r_distance = 1.0 if v > 0 else 0.0 #andar em frente +1
 
-        min_range = np.min(obs[140:220])
+        min_range = np.min(obs[140:220]) #raios frontais
         collision_threshold = 1.0
 
         if min_range < collision_threshold:
-            r_collision = -np.exp(3 * (collision_threshold - min_range)) + 1
+            r_collision = -np.exp(3 * (collision_threshold - min_range)) + 1 #penalização mais negativa quanto mais perto
         else:
             r_collision = 0.0
 
-        r_navigation = self.navigation_reward(obs, action)
-        r_stability = self.stability_reward(obs, action)
+        r_navigation = self.navigation_reward(obs, action) #recompensa por direção adequada
+        r_stability = self.stability_reward(obs, action) #penalização por virar
 
         return r_distance + r_collision + r_navigation + r_stability
 
@@ -115,31 +118,39 @@ class WheelchairEnv(gym.Env):
         front_sector = obs[170:190]
         right_sector = obs[190:260]
 
-        left_clearance = np.mean(left_sector)
+        left_clearance = np.mean(left_sector) #dist média livre à esquerda
         right_clearance = np.mean(right_sector)
 
+        #pelo menos uma leitura frontal a menos de 2.5
         obstacle_ahead = np.any(front_sector < self.commitment_threshold)
 
         if obstacle_ahead:
+            # >0 direita mais livre, <0 esquerda mais livre
             clearance_diff = right_clearance - left_clearance
 
+            #60% da preferência anterior, 40% da diferença atual
             alpha = 0.40
             self.prev_pref = (1 - alpha) * self.prev_pref + alpha * clearance_diff
 
             r = 3.0
 
+            #pref direita mais livre
             if self.prev_pref > 0.2:
                 return r if w < 0 else -r
 
+            #pref esquerda mais livre
             if self.prev_pref < -0.2:
                 return r if w > 0 else -r
 
+            #espaço direita mais livre
             if clearance_diff > 0.2:
                 return r if w < 0 else -r * 0.5
-
+            
+            #espaço esquerda mais livre
             if clearance_diff < -0.2:
                 return r if w > 0 else -r * 0.5
 
+            #perto e não vira
             if np.min(front_sector) < 1.0:
                 return r if w != 0 else -r
 
@@ -151,6 +162,7 @@ class WheelchairEnv(gym.Env):
     def stability_reward(self, obs: np.ndarray, action: Tuple[int, int]) -> float:
         _, w = action
 
+        #se vira pequena penalização
         if w != 0:
             return -0.2
 
@@ -166,8 +178,8 @@ class WheelchairEnv(gym.Env):
         return 0
 
     def send_action_get_obs(self, action: Tuple[int, int]) -> RobotState:
-        self.socket.send_pyobj(action)
-        return self.get_observation()
+        self.socket.send_pyobj(action) #eviar ação
+        return self.get_observation() #esperar resposta
 
     def get_observation(self) -> RobotState:
         state = self.socket.recv_pyobj()
